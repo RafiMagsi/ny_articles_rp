1f85b72c3c6ba3d3b46f6bfd5e86782963ceaee7
Rafi Magsi <muhammed.rafi@taifco.net>
2024-10-15 13:20:30 +0400

    Unused dependency removed

--- lib/data_layer/data_providers/home_viewmodel.dart
+++ lib/data_layer/data_providers/home_viewmodel.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ny_articles_rp/application_layer/services/article_service.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 final periodIdProvider = Provider<String>((_) => '1');
 
